c70830b3987c9c159b270261bb4dfd0ad892460a
Vipul Kumar <vipulk772@gmail.com>
2023-10-14 22:40:52 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'vipulk772',
+    author_email= 'vipulk772@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
