59cc13ec865840419541564947ac58a53928d8c2
Vipul Kumar <vipulk772@gmail.com>
2023-10-15 19:00:55 +0530

    data validation done

--- cellSegmentation/pipeline/training_pipeline.py
+++ cellSegmentation/pipeline/training_pipeline.py
@@ -2,20 +2,22 @@ import sys, os
 from cellSegmentation.logger import logging
 from cellSegmentation.exception import AppException
 from cellSegmentation.components.data_ingestion import DataIngestion
-#from cellSegmentation.components.data_validation import DataValidation
+from cellSegmentation.components.data_validation import DataValidation
 #from cellSegmentation.components.model_trainer import ModelTrainer
 
 
-from cellSegmentation.entity.config_entity import (DataIngestionConfig)
+from cellSegmentation.entity.config_entity import (DataIngestionConfig,
+                                                 DataValidationConfig)
 
-from cellSegmentation.entity.artifacts_entity import (DataIngestionArtifact)
+from cellSegmentation.entity.artifacts_entity import (DataIngestionArtifact,
+                                                    DataValidationArtifact)
 
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-#        self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
  #       self.model_trainer_config = ModelTrainerConfig()
 
 
@@ -45,6 +47,34 @@ class TrainPipeline:
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact=self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
         except Exception as e:
             raise AppException(e,sys)
+        
+
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AppException(e, sys) from e
